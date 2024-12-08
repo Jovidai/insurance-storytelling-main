5eb134c2a484975b2715e614c37614c5310f63e6
Jovidai <daizhaozhou1999@gmail.com>
2024-12-08 11:00:28 -0800

    updated duration

--- config.js
+++ config.js
@@ -97,12 +97,12 @@ var config = {
                 {
                     layer: 'ca-county-boundaries',
                     opacity: 0,
-                    duration: 900
+                    duration: 1000
                 },
                 {
                     layer: 'ca-housing-insurance',
                     opacity: 0,
-                    duration: 900
+                    duration: 1000
                 },
             ]
         },
@@ -130,12 +130,12 @@ var config = {
                 {
                     layer: 'ca-county-boundaries',
                     opacity: .8,
-                    duration: 900
+                    duration: 1000
                 },
                 {
                     layer: 'ca-housing-insurance',
                     opacity: .8,
-                    duration: 900
+                    duration: 1000
                 },
             ],
             onChapterExit: []
